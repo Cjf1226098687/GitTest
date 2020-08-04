512801e78528a141df8b6718affb0198d822d8b0
CJF <1226098687@qq.com>
2020-08-04 10:46:14 +0800

    第五次修改

--- src/main/java/com/qfedu/test/GitTest.java
+++ src/main/java/com/qfedu/test/GitTest.java
@@ -1,13 +1,15 @@
 package com.qfedu.test;
 
+import org.junit.Test;
+
 /**
  * @author Buffer
  * @date 2020/8/4 9:49
- * @Description
+ * @Description 测试 Git
  */
 public class GitTest {
-    public static void main(String[] args) {
-        System.out.println("Git");
-        System.out.println("Git");
+    @Test
+    public void demo() {
+        System.out.println("This is Git Test");
     }
 }
