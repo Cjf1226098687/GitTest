0908c3b76ba41140b51143f550d6cd235562bc2e
CJF <1226098687@qq.com>
2020-08-04 10:02:52 +0800

    第三次提交

--- src/main/java/com/qfedu/test/GitTest.java
+++ src/main/java/com/qfedu/test/GitTest.java
@@ -6,4 +6,8 @@ package com.qfedu.test;
  * @Description
  */
 public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Git");
+        System.out.println("Git");
+    }
 }
