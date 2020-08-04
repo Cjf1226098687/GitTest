f4df239e539a9335eacc0a6d19f16be0f8b79260
Buffer <45114230+Cjf1226098687@users.noreply.github.com>
2020-08-04 17:48:55 +0800

    Update User.java

--- src/main/java/com/qfedu/pojo/User.java
+++ src/main/java/com/qfedu/pojo/User.java
@@ -3,7 +3,7 @@ package com.qfedu.pojo;
 /**
  * @author Buffer
  * @date 2020/8/4 17:04
- * @Description
+ * @Description 测试 Git 的comiit、pull、push
  */
 public class User {
     private String name;
